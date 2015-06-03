095e2850b63d908658d44898d5c563fe712cdc3e
Rohit Sardesai <rsardesai@edgewaternetworks.com>
2015-06-03 12:09:57 +0530

    Added maven-sql plugin to create / drop test db schema / tables before / after tests are run.

---
+++ dbunit/src/main/resources/create-employeedb.sql
@@ -0,0 +1,5 @@
+-- Used to create the testemployeedb schema for the unit tests
+DROP DATABASE IF EXISTS testemployeedb;
+CREATE DATABASE IF NOT EXISTS testemployeedb;
+USE testemployeedb;
+
--- dbunit/src/test/java/com/sample/spring/dbunit/EmployeeDaoTest.java
+++ dbunit/src/test/java/com/sample/spring/dbunit/EmployeeDaoTest.java
@@ -3,6 +3,7 @@ package com.sample.spring.dbunit;
 import java.util.List;
 
 import junit.framework.Assert;
+import junit.framework.TestCase;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,21 +14,24 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 
 import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseOperation;
 import com.github.springtestdbunit.annotation.DatabaseSetup;
+import com.github.springtestdbunit.annotation.DatabaseTearDown;
 import com.sample.spring.dbunit.dao.EmployeeDao;
 import com.sample.spring.dbunit.dao.exception.DaoException;
 import com.sample.spring.dbunit.domain.Employee;
 
-@DatabaseSetup("classpath:dbunit-employee.xml")
+@DatabaseSetup(value="classpath:dbunit-employee.xml", type=DatabaseOperation.CLEAN_INSERT)
+@DatabaseTearDown(value="classpath:dbunit-employee.xml", type=DatabaseOperation.DELETE_ALL)
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath:applicationContext.xml")
 @TestExecutionListeners({ DependencyInjectionTestExecutionListener.class,
-		DbUnitTestExecutionListener.class })
-public class EmployeeDaoTest {
+						  DbUnitTestExecutionListener.class })
+public class EmployeeDaoTest extends TestCase {
 
 	@Autowired
 	private EmployeeDao employeeDao;
-
+	
 	@Test
 	public void getAllEmployees() throws DaoException {
 		List<Employee> employees = employeeDao.findAll();
